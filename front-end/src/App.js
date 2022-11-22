926cb2aed489c4e5a8203f104e925853591210ce
sebasthub <sebastiaonogueira@unitins.br>
2022-11-21 22:41:00 -0300

    tabela integrada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,41 @@
+import { Table } from '@mantine/core';
+import { useEffect, useState } from "react";
+
+
+
 function App() {
+  const [elements, setElements] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/manutencoes`).then((response) => response.json()).then((character) => setElements(character.response));
+  });
+  const rows = elements.map((element) => (
+    <tr key={element.idmanutencao}>
+      <td>{element.data.split("T")[0]}</td>
+      <td>{element.solicitante}</td>
+      <td>{element.veiculo}</td>
+      <td>{element.empresa_manutencao}</td>
+      <td>{element.retorno.split("T")[0]}</td>
+      <td>{element.valor}</td>
+      <td>{element.problemas}</td>
+    </tr>
+  ));
+
   return (
-    <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
+    <Table>
+      <thead>
+        <tr>
+          <th>data</th>
+          <th>solicitante</th>
+          <th>veiculo</th>
+          <th>empresa realizadora</th>
+          <th>data de retorno</th>
+          <th>valor</th>
+          <th>problemas</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
   );
 }
 
